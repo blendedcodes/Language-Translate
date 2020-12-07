5bbe3a7384be87918506043e50b679da3a761f0f
Onoriode Gideon A <jenax2onox@gmail.com>
2020-12-07 12:30:18 +0100

    First commit for this project

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="eng">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>PHP TEST PAGE</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <style>
+.translated-ltr{margin-top:-40px;}
+.translated-ltr{margin-top:-40px;}
+.goog-te-banner-frame {display: none;margin-top:-20px;background: lightgrey;}
+
+.goog-logo-link {
+   display:none !important;
+} 
+
+.goog-te-gadget{
+   color: transparent !important;
+   
+} 
+</style>
+    </head>
+    <body>
+
+<div id="google_translate_element_id"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element_id');
+}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<p>
+            PHP Associative Arrays
+An associative array is similar to an indexed array, but rather than storing data sequentially with numeric index, every value can be assigned with a user-designed key of string type.
+        </p>
+</body>
+</html>
